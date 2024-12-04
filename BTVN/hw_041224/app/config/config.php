3c2bca5deb5a859f27d75bd63b0736fd79b1264e
huynq2004 <huynguyenfw@gmail.com>
2024-12-04 10:21:43 +0700

    cấu hình định tuyến

---
+++ BTVN/hw_041224/app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+define('APP_ROOT', dirname(__FILE__,3));
+define('DOMAIN', 'http://localhost/School-WebEngineering-2024/BTVN/hw_041224/');
+
+//Dinh nghia hang so ket noi CSDL
+define ('DB_HOST','localhost');
+define ('DB_USER','root');
+define ('DB_PASS','');
+define ('DB_NAME','products');
